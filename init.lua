f85fc69ea2d32c1de42d85f71fcf60fe8e592984
elnurastanov <elnurastanov@gmail.com>
2023-02-06 12:35:19 +0400

    Initial commit

---
+++ init.lua
@@ -0,0 +1,6 @@
+require("core.keymaps")
+require("core.options")
+require("plugins.packer")
+require("plugins.telescope")
+require("plugins.gitsigns")
+require("plugins.theme")
---
+++ lua/plugins/theme.lua
@@ -0,0 +1,7 @@
+local theme = require("onedark")
+
+theme.setup {
+	style = "warm"
+}
+
+theme.load()
