9d7e75da740cf0a7acc959065e6c947a3395b752
elnurastanov <elnurastanov@gmail.com>
2023-02-08 14:25:24 +0400

    Added lualine plugin

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -12,4 +12,8 @@ return require('packer').startup(function(use)
 	
 	use 'navarasu/onedark.nvim'
 
+	use {
+		'nvim-lualine/lualine.nvim',
+		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+	}
 end)
