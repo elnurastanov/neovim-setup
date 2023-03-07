b58a12c54c0b4e7c832afa1cf84ddd0b62468446
Elnur Astanov <54953391+elnurastanov@users.noreply.github.com>
2023-03-07 08:47:28 +0400

    Added live grep search

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,6 +4,7 @@ if (not status) then return end
 local builtin = require("telescope.builtin")
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fs', builtin.live_grep, {})
 
 telescope.setup {
   defaults = {
