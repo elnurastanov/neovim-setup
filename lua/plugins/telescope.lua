f136031a10acef7dd3486d9c9c746b083cdfa757
Elnur Astanov <54953391+elnurastanov@users.noreply.github.com>
2023-03-09 15:52:42 +0400

    Updated telescope config
    Resized ui width and added git changed files keymap

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,17 +5,28 @@ local builtin = require("telescope.builtin")
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fs', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
 
 telescope.setup {
-  defaults = {
-    file_ignore_patterns = {
-      "node_modules",
-      ".git",
-    }
-  },
-  pickers = {
-    find_files = {
-      hidden = true,
-    }
-  }
+	defaults = {
+		file_ignore_patterns = {
+			"node_modules",
+			".git",
+		},
+	},
+	pickers = {
+		find_files = {
+			hidden = true,
+			theme = "dropdown",
+			layout_config = {
+				width = 0.7
+			},
+		},
+		live_grep = {
+			theme = "dropdown",
+			layout_config = {
+				width = 0.7
+			},
+		}
+	}
 }
