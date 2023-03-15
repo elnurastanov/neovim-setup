45288286fd1d518a707f9df2110df56a3c373c9d
Elnur Astanov <54953391+elnurastanov@users.noreply.github.com>
2023-03-15 14:00:32 +0400

    Configured mason-null-ls plugin

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -4,7 +4,15 @@ if (not status) then return end
 local status2, mason_lsp = pcall(require, "mason-lspconfig")
 if (not status2) then return end
 
+local status3, mason_null_ls = pcall(require, "mason-null-ls")
+if (not status3) then return end
+
 mason.setup {}
+
 mason_lsp.setup {
-	ensure_installed = { "tsserver", "jsonls" }
+	ensure_installed = { "tsserver", "jsonls" },
+}
+
+mason_null_ls.setup {
+	ensure_installed = { "prettierd" },
 }
